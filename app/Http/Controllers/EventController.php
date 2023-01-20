e9df1790c3f7375e830441ce362d6ddb222e650b
FrsErley <frsleeh@gmail.com>
2023-01-20 00:54:59 -0300

    Projeto laravel finalizado !

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -59,6 +59,12 @@ class EventController extends Controller
 
             $event->image = $imageName;
 
+        } else {
+
+            $defaultImage = 'event_placeholder.jpg';
+
+            $event->image = $defaultImage;
+
         }
 
         $user = auth()->user();
@@ -78,9 +84,24 @@ class EventController extends Controller
 
         $event = Event::findorFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user) {
+
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent) {
+            if($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event'=> $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event'=> $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
 
     }
 
@@ -90,7 +111,11 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+    
+        return view('events.dashboard', 
+        ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]
+    );
 
     }
 
@@ -103,9 +128,15 @@ class EventController extends Controller
 
     public function edit($id) {
 
+        $user = auth()->user();
+
         $event = Event::findorFail($id);
 
         return view('events.edit', ['event' => $event]);
+
+        if($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
 
     }
 
@@ -131,6 +162,30 @@ class EventController extends Controller
         Event::findorFail($request->id)->update($data);
 
         return redirect('/dashboard')->with('msg','Evento editado com sucesso!');
+
+    }
+
+    public function joinEvent($id) {
+
+        $user = auth()->user(); 
+
+        $user->eventsAsParticipant()->attach($id);
+
+        $event = Event::findorFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento' . $event->title);
+
+    }
+
+    public function leaveEvent($id) {
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Event::findorFail($id); 
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento:' . $event->title);
 
     }
 
